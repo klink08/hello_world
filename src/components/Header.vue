74ff6196668e72d7a2c90ab7e080e946300975aa
Klink <klink008@gmail.com>
2025-05-23 10:34:09 -0400

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useUserStore } from "@/stores/user.js";
+
+const userStore = useUserStore();
+</script>
+
+<template>
+  <main>
+    <div>{{userStore.firstName}}</div>
+    <div>{{userStore.lastName}}</div>
+    <div>{{userStore.fullName()}}</div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+
+createApp(App).mount('#app')
