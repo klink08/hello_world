1f6b95439d99b68b146c58560c4eae5f74d60c90
Klink <klink008@gmail.com>
2026-04-19 17:43:26 -0400

    Trying to fix the deployment.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 import magicalSvgPlugin from 'vite-plugin-magical-svg'
 
 export default defineConfig({
-  base: '/',
+  base: '/vue-example/',
   plugins: [
     vue(),
     vueDevTools(),
